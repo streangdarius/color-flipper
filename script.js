4fb2e8ff6dcb603d299fd4945bc07ccc2644416b
Darius Streang <streangdarius@gmail.com>
2022-04-16 13:03:00 +0300

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+// Random Color Generator
+let colorText = document.getElementById("color-name");
+let flipperButton = document.querySelector(".color-button");
+let backButton = document.querySelector(".back-button");
+
+const flipColor = () => {
+  let colorFlipper = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.background = "#" + colorFlipper;
+  colorText.innerText = "The color code is: #" + colorFlipper;
+};
+
+flipperButton.addEventListener("click", flipColor);
+
+flipColor();
+
+// Back button error
+backButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Not yet ready. Wait a couple of weeks until then.");
+});
+
+console.warn(
+  "If you want my opinion, I think people should keep their opinions to themselves. 😎",
+  "\"Snoops Toping\""
+);
